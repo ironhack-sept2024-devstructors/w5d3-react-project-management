d0b36f9fe3a9d8cde71c8d24b0fc6b39c709ffdc
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-10-30 11:46:07 +0100

    implement CreateProjectPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import HomePage from "./pages/HomePage"
 import ProjectListPage from "./pages/ProjectListPage"
+import CreateProjectPage from "./pages/CreateProjectPage"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/projects" element={<ProjectListPage />} />
+        <Route path="/projects/create" element={<CreateProjectPage />} />
         <Route path="/*" element={<h2>Page not found</h2>} />
       </Routes>
     </>
---
+++ src/pages/CreateProjectPage.jsx
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { API_URL } from "../config/api";
+
+function CreateProjectPage() {
+
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        const newProject = {
+            title: title,
+            description: description
+        }
+
+        axios.post(`${API_URL}/projects/`, newProject)
+            .then( response => {
+                navigate("/projects"); // redirect to projects page
+            })
+            .catch(e => console.log("Error creating a new project...", e));
+    }
+
+    return (
+        <div className="CreateProjectPage">
+            <h3>Add Project</h3>
+
+            <form onSubmit={handleSubmit}>
+
+                <label>
+                    Title:
+                    <input 
+                        type="text" 
+                        name="title" 
+                        placeholder="enter the title" 
+                        required
+                        value={title}
+                        onChange={(e) => { setTitle(e.target.value) }}
+                    />
+                </label>
+
+                <label>
+                    Description: 
+                    <input 
+                        type="text" 
+                        name="description" 
+                        placeholder="enter the description" 
+                        value={description}
+                        onChange={(e) => { setDescription(e.target.value) }}
+                    />
+                </label>
+
+                <button>Create</button>
+            </form>
+        </div>
+    );
+}
+
+export default CreateProjectPage;
